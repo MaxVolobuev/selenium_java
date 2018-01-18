2c11dbbbc1ad84bce8cd92b86060ccdf6bbdf730
Max Volobuev <maxvolobuev@icloud.com>
2018-01-18 18:35:19 +0200

    Selenium Test

---
+++ FacebookLoginTest.java
@@ -0,0 +1,36 @@
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.LoggedInUserManPage;
+import utils.InitPageElements;
+import utils.Navigation;
+import utils.WebDriverManager;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.testng.Assert.assertTrue;
+
+
+public class FacebookLoginTest {
+
+
+    @BeforeMethod
+    public void beforeEachtests(){
+        WebDriverManager.openBrowser();
+    }
+
+
+    @Test
+    public void successFacebookLoginTest(){
+      LoggedInUserManPage loggedInUserManPage = Navigation.openFacebookMainPage().submitForCredentials("maxvolobuev@icloud.com", "90gepard");
+      assertTrue(loggedInUserManPage.isSearchFieldPresent());
+
+    }
+
+    @AfterMethod
+    public void afterEachTest(){
+        WebDriverManager.closeBrowser();
+    }
+}
